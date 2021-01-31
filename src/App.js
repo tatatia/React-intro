4ddd3b323091b1c1b0ba40503ee9632ce64dcd9a
tatatia <thetattianochka@gmail.com>
2021-01-31 21:55:54 +0200

    added two blocks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pre-menu">
+        <h1 id="title">Тестове завдання</h1>
+      </div>
+      <div className="menu">
+        <a href="#">Головна</a>
+        <a href="#description">Опис проекту</a>
+        <a href="#about-author">Про автора</a>
+        <a href="#about-author">Контакти</a>
+      </div>
     </div>
   );
 }
