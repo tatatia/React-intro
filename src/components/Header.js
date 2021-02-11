bb138fe53b8184263279c57980feb6f1f5937e60
tatatia <thetattianochka@gmail.com>
2021-02-11 22:56:20 +0200

    added function setActiveItem

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeItem: "Опис проекту"
+        }
+
+        this.setActiveItem = this.setActiveItem.bind(this);
+    }
+
+    setActiveItem(item) {
+        this.setState(state => ({
+            activeItem: item.text
+        }))
+    }
+
+    render() {
+        return (
+            <div className="menu">
+                {this.props.items.map(item =>
+                    <a
+                        onClick={() => this.setActiveItem(item)}
+                        className={(item.text == this.state.activeItem) ? "active" : ""}
+                        href={item.link}
+                    >{item.text}</a>)}
+            </div>
+        )
+    }
+}
+
 function Header(props) {
     return (
         <>
             <div className="pre-menu">
                 <h1 id="title">{props.title}</h1>
             </div>
-            <div className="menu">
-                {props.menu.map(item => <a href={item.link}>{item.text}</a>)}
-            </div>
+            <Menu items={props.menu} />
         </>
     );
 }
