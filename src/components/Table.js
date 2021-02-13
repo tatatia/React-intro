338a152056ff5bf46323d0729783d411494c3618
tatatia <thetattianochka@gmail.com>
2021-02-13 19:51:04 +0200

    added map in table

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 class Table extends React.Component {
     render() {
+        let arr = [
+            { name: "Taras", year: 1993 },
+            { name: "Ivan", year: 2010 },
+            { name: "Tanya", year: 1990 },
+            { name: "Tony", year: 1965 }
+        ]
         return (
-            <h1>Hello</h1>
+            <div className="work-books">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Year</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {arr.map(item => <tr><td>{item.name}</td><td>{item.year}</td></tr>)}
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
