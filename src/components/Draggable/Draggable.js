5df8bd2cb5ed8ee4163cfcc60a5d24e70a7d2a3c
tatatia <thetattianochka@gmail.com>
2021-05-03 13:22:19 +0300

    changed Draggable to functional

--- src/components/Draggable/Draggable.js
+++ src/components/Draggable/Draggable.js
@@ -1,61 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/Draggable.css'
 import PropTypes from 'prop-types'
 
+function Draggable({ translate, tasksList }) {
+    const [tasks, setTasks] = useState(tasksList)
+    const [taskInFly, setTaskInFly] = useState("")
 
-class Draggable extends React.Component {
-    constructor(props) {
-        super(props);
-        const { tasks } = props;
-        this.state = {
-            tasks,
-            taskInFly: ""
-        }
+    const handleDragStart = (event, task) => {
+        setTaskInFly(task)
     }
 
-    handleDragStart = (event, task) => {
-        this.setState({ taskInFly: task })
-    }
-
-    handleDragOver = event => {
+    const handleDragOver = event => {
         event.preventDefault()
         event.stopPropagation()
     }
 
-    handleDrop = (event, task) => {
-        const { tasks, taskInFly } = this.state;
+    const handleDrop = (event, task) => {
         const tmpTasks = [...tasks]
         const fromIndex = tmpTasks.findIndex((elem) => elem === taskInFly)
         const toIndex = tmpTasks.findIndex((elem) => elem === task)
         tmpTasks.splice(fromIndex, 1)
         tmpTasks.splice(toIndex, 0, taskInFly)
-        this.setState({ tasks: tmpTasks })
+        setTasks(tmpTasks)
     }
 
-    render() {
-        const { tasks } = this.state
-        const { translate } = this.props
-        return (
-            <div className="work-books">
-                <section className="tasks">
-                    <h1 className="tasks__title">{translate("Task list")}</h1>
-                    <ul className="tasks__list">
-                        {tasks.map((task) =>
-                            <li key={task} className="tasks__item"
-                                draggable="true"
-                                onDrop={event => this.handleDrop(event, task)}
-                                onDragOver={event => this.handleDragOver(event)}
-                                onDragStart={event => this.handleDragStart(event, task)}
-                            > {translate(task)} </li>
-                        )}
-                    </ul>
-                </section>
-            </div>
-        )
-    }
+    return (
+        <div className="work-books">
+            <section className="tasks">
+                <h1 className="tasks__title">{translate("Task list")}</h1>
+                <ul className="tasks__list">
+                    {tasks.map((task) =>
+                        <li key={task} className="tasks__item"
+                            draggable="true"
+                            onDrop={event => handleDrop(event, task)}
+                            onDragOver={event => handleDragOver(event)}
+                            onDragStart={event => handleDragStart(event, task)}
+                        > {translate(task)} </li>
+                    )}
+                </ul>
+            </section>
+        </div>
+    )
 }
+
 Draggable.propTypes = {
-    tasks: PropTypes.array,
+    tasksList: PropTypes.array,
     lang: PropTypes.string
 }
 export default Draggable
